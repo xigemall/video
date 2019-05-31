3e62986d51f6a1fcc370a6295342a701b74d7aae
liuyong <1032380143@qq.com>
2019-05-31 20:52:32 +0800

    代理服务端api地址改为localhost

--- config/proxy.config.js
+++ config/proxy.config.js
@@ -1,6 +1,6 @@
 export default {
     "/api": {
-        "target": "http://192.168.31.8:8002/",
+        "target": "http://localhost:8002/",
         "changeOrigin": true,
         "pathRewrite": { "^/api" : "" }
     }
